1f7ea2f7c36b2d2d7f3343eb452b924c6f75e964
Ranjita16-thami <thami.ranjita63@gmail.com>
2026-01-10 16:45:19 +0545

    db and tools setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+ import { ClerkProvider } from '@clerk/clerk-react' // now we r rdy to use this
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing  Clerk Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+         <App /> 
+      </ClerkProvider>
+   
   </StrictMode>,
 )
+
+// in <App/> //wrapping our entire application with clerkprovider so that in our entire app we can use clerk we can use all the function all the hooks companies coming from the clerk
